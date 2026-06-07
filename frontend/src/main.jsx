ecfae16ab9c57bcafe8db8f6fd8f48e5e5293448
Toff-cowan <cowanchristoff24@gmail.com>
2026-06-06 19:52:31 -0500

    anayltics dependencies

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import { CountdownVisibilityProvider } from "./context/CountdownVisibilityContext.jsx";
@@ -14,6 +15,7 @@ createRoot(document.getElementById("root")).render(
         <ThemeProvider>
           <CountdownVisibilityProvider>
             <App />
+            <Analytics />
           </CountdownVisibilityProvider>
         </ThemeProvider>
       </AuthProvider>
